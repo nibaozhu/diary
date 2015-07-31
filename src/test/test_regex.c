d3a3442594a57bbeef417ed86e2d2cb8e95819eb
nibaozhu <nibz@qq.com>
2015-07-31 15:03:11 +0800

    align code

--- src/test/test_regex.c
+++ src/test/test_regex.c
@@ -11,9 +11,9 @@
 #include <string.h>
 
 
-int main(void)
+int main(int argc, char **argv)
 {
-	//int regcomp(regex_t *preg, const char *regex, int cflags);
+//	int regcomp(regex_t *preg, const char *regex, int cflags);
 	regex_t preg;
 	const char *regex = "http://\\([a-z0-9]\\+\\.\\)\\+[a-z0-9]\\+\\(:[0-9]\\+\\)\\?\\(/[a-z0-9\\.\\?=&]\\+\\)*\\(/\\)\\?";
 	int cflags = REG_ICASE;
@@ -26,25 +26,22 @@ int main(void)
 	errcode = regcomp(&preg, regex, cflags);
 	if (errcode != 0)
 	{
-// size_t regerror(int errcode, const regex_t *preg, char *errbuf,
+//	size_t regerror(int errcode, const regex_t *preg, char *errbuf,
 //                 size_t errbuf_size);
-
 		size = regerror(errcode, &preg, errbuf, errbuf_size);
 		printf("errcode = %d, %s(%u)\n", errcode, errbuf, size);
 		return 1;
 	}
 
-// int regexec(const regex_t *preg, const char *string, size_t nmatch,
-//                   regmatch_t pmatch[], int eflags);
-
-	//const char *string = "how are you? yes. http://www9.sina.com.cn:80/news/index.html hi, very good.\n";
 	const char *string = "<a data-href=\"http://news.baidu.com\" \r\nhref=\"http://news.baidu.com/ns?word=word&tn=news&cl=2&rn=20&ct=1&fr=wenku\" class=\"logSend\" data-lojgsend='{\"send\":[\"general\", ";
-	regmatch_t pmatch[10];
-	size_t nmatch = 10;
+	regmatch_t pmatch[1];
+	size_t nmatch = 1;
 	int eflags = REG_NOTBOL;
 
 	while (errcode == 0)
 	{
+//	int regexec(const regex_t *preg, const char *string, size_t nmatch,
+//                   regmatch_t pmatch[], int eflags);
 		// regexec() returns zero for a successful match or REG_NOMATCH for failure.
 		errcode = regexec(&preg, string, nmatch, pmatch, eflags);
 		if (errcode == 0)
@@ -60,7 +57,6 @@ int main(void)
 
 //	void regfree(regex_t *preg);
 	regfree(&preg);
-
 	return 0;
 }
 
