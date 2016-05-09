75e3f728328f0ab90ea7b02fee2150c30e468a22
Ni Baozhu <nibz@qq.com>
2016-05-09 15:53:51 +0800

    init

---
+++ src/proXy/handle.h
@@ -0,0 +1,14 @@
+/*
+ * This is free software; see the source for copying conditions.  There is NO
+ * warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+#ifndef HANDLE_H
+#define HANDLE_H
+
+#include "transport.h"
+
+
+int handle(std::list<Transport*> *w, std::map<int, Transport*> *m, std::map<uint32_t, int> *__m, Transport* t);
+
+#endif
+
---
+++ src/proXy/version.h
@@ -0,0 +1,11 @@
+/*
+ * This is free software; see the source for copying conditions.  There is NO
+ * warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+const char *author = "Ni BaoZhu";
+const char *email = "nibz@qq.com";
+const char *home = "https://github.com/nibaozhu/mycode";
+
+/* {experimental, unstable, stable} */
+const char *version = "1.0-unstable";
