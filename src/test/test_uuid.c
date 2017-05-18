5c6548e5c40721e6e7cf69ed73da378121a0bb9c
nibaozhu <nibz@qq.com>
2017-05-19 01:14:32 +0800

    add uuid

---
+++ src/test/test_uuid.c
@@ -0,0 +1,44 @@
+// uuid.h, libuuid.a, libuuid.so.x.x.x
+// $ cc -o test_uuid test_uuid.c /path/to/libuuid.a
+// $ cc -o test_uuid test_uuid.c /path/to/libuuid.so.x.x.x
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "uuid.h"
+
+int main()
+{
+
+  uuid_t *_uuid = NULL;
+  uuid_rc_t rc = uuid_create(&_uuid);
+  if (rc != UUID_RC_OK) return 0;
+
+  const char *_name = "nil";
+  rc = uuid_load( _uuid, _name);
+  if (rc != UUID_RC_OK) return 0;
+
+  int i;
+  for (i = 0; i < 1024 * 1024; i++)
+  {
+    unsigned int _mode = UUID_MAKE_V1;
+    rc = uuid_make(_uuid, _mode);
+    if (rc != UUID_RC_OK) return 0;
+
+    uuid_fmt_t _fmt = UUID_FMT_STR;
+    void *_data_ptr = NULL;
+    size_t _data_len = 0;
+    rc = uuid_export(_uuid, _fmt, &_data_ptr, &_data_len);
+    if (rc != UUID_RC_OK) return 0;
+
+    fprintf(stdout, "%s\n", _data_ptr);
+    free(_data_ptr);
+    _data_ptr = NULL;
+  }
+
+  rc = uuid_destroy(_uuid);
+  if (rc != UUID_RC_OK) return 0;
+
+  return 0;
+}
+
