3dc039dc1328101ec74bf181f3be987803d2d34f
nibaozhu <nibz@qq.com>
2017-08-04 22:47:19 +0800

    fix some warning

--- src/test/test_string.cc
+++ src/test/test_string.cc
@@ -18,12 +18,14 @@ void string2vector(const std::string &str, const char &delim, std::vector<std::s
 
 
 int main(int argc, char **argv) {
+	(void)argc;
+	(void)argv;
 	std::string s1("tcp:1.2.3.4:5:6");
 	std::vector<std::string> v1;
 
 	string2vector(s1, ':', v1);
 
-	int i;
+	size_t i;
 	for(i = 0; i < v1.size(); i++)
 		std::cout << "i: "<< i << ",\t" << v1.at(i) << std::endl;
 
