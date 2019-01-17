31542d122249d58b114041f6eab27596c8553482
Ni Baozhu <nibz@qq.com>
2019-01-17 22:19:22 +0800

    support zero and negative number

--- src/test/p1/35.py
+++ src/test/p1/35.py
@@ -5,13 +5,16 @@ def collatz(number):
 	if number % 2 == 0:
 		number = number//2
 	else:
-		number = 3*number+1
+		if number > 0:
+			number = 3*number+1
+		else:
+			number = 3*number-1
 	print(number)
 	return number
 
 def call_me(number):
 	print(number)
-	while number != 1:
+	while number != 1 and number != 0 and number != -1:
 		number = collatz(number)
 
 call_me(int(input(">> ")))
